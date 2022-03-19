31a9db02475f5828c01754e88382d3cab34bb07c
Cameron <Cam@thepinks.net>
2022-03-18 23:46:48 -0600

    NoteReader Script created.
    Reads Title, BPM, and then outputs notes to the console.

--- GameJamMarch22/Assets/Scripts/NoteReader.cs
+++ GameJamMarch22/Assets/Scripts/NoteReader.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class NoteReader : MonoBehaviour
 {
+    public string stringpath;
+    public StreamReader reader;
+    float BPM;
+
     // Start is called before the first frame update
     void Start()
     {
+        reader = new StreamReader(Application.persistentDataPath + stringpath);
+        BeginRead();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    //Grabs the speed of the song and starts the iteration
+    void BeginRead()
     {
-        
+        Debug.Log(reader.ReadLine());
+        BPM = float.Parse(reader.ReadLine());
+        Debug.Log(BPM.ToString());
+        ReadBars();
+    }
+
+    void ReadBars()
+    {
+        if (reader.Peek() > -1)
+        {
+            Debug.Log(reader.ReadLine());
+            StartCoroutine(BarDelay());
+        }
+    }
+
+    public IEnumerator BarDelay()
+    {
+        yield return new WaitForSecondsRealtime(1 / (BPM / 60));
+        ReadBars();
     }
 }
